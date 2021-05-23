e5db94fec7856da6765a7109f6a545f7d3215174
aman1505 <aman.jain1505@gmail.com>
2021-05-23 11:22:54 +0530

    remove duplicate from list

--- 26-RemoveDuplicatesFromSortedArray.py
+++ 26-RemoveDuplicatesFromSortedArray.py
@@ -2,12 +2,24 @@ from typing import  List
 
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int :
-        count=0
-        for i in range(len(nums)):
-            if nums[i]==nums[i+1]:
-                nums[i+1] = nums[i+2] 
-                count = count+1
-        return count+1
+        # count=0
+        i=0
+        while (i < len(nums)-1):
+            if(nums[i]==nums[i+1]):
+                del(nums[i])
+                # count+=1
+            else:
+                i+=1
+        return len(nums)
+            
+
+        # for i in range(len(nums)):
+        #     if (i+1 == len(nums)):
+        #         break
+        #     if nums[i]==nums[i+1]:
+        #         nums.remove(nums[i])
+        #         count = count+1
+        # return count+1, nums
 
 
 if __name__ == "__main__":
@@ -15,3 +27,8 @@ if __name__ == "__main__":
     obj = Solution()
     result = obj.removeDuplicates(nums)
     print(result)
+    
+
+
+#i=0 nums = [0,1,1,1,1,2,2,3,3,4]
+#i=1 nums = [0,1,1,1,1,2,2,3,3,4]
